b5421de7a796a973ddddb5b012227892a0d1d571
Alex Karumbi <alex.muhia@student.moringaschool.com>
2021-09-05 21:03:27 +0300

    Add decryption logic

--- src/main/java/CaesarCipherDecrypt.java
+++ src/main/java/CaesarCipherDecrypt.java
@@ -8,10 +8,17 @@ public class CaesarCipherDecrypt {
         for (int counter =0;counter<s.length();counter++){
             if (!Character.isLetter(s.charAt(counter))){
                 cypherText.append(s.charAt(counter));
+            }else{
+                int charPosition=ALPHABETS.indexOf(s.charAt(counter));
+                int keyValue = (charPosition-shiftKey) % 25;
+                if (keyValue < 0){
+                    keyValue =ALPHABETS.length()-1+keyValue;
+                }
+                char cypherValue=ALPHABETS.charAt(keyValue);
+                cypherText.append(cypherValue);
             }
         }
-
-
-
+        String z=cypherText.toString();
+        return z;
     }
 }
