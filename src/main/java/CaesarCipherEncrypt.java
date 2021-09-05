74ad01c9544b72b7ddcd6d47020a8b0c317772a2
Alex Karumbi <alex.muhia@student.moringaschool.com>
2021-09-05 20:42:03 +0300

    Add encryption logic

--- src/main/java/CaesarCipherEncrypt.java
+++ src/main/java/CaesarCipherEncrypt.java
@@ -6,4 +6,25 @@ public class CaesarCipherEncrypt {
     public int getShiftKey(){return shiftKey;}
     public void setMessage(String message) { this.message = message; }
     public void setShiftKey(int shiftKey) { this.shiftKey = shiftKey; }
+
+    public static String CaesarCipherEncryption(String message,int shiftKey){
+        final String ALPHABETS="abcdefghijklmopqrstuvwxyz";
+        message=message.toLowerCase();
+
+        StringBuilder cypherText=new StringBuilder();
+        for (int counter =0;counter<message.length();counter ++){
+            if (!Character.isLetter(message.charAt(counter))){
+                cypherText.append(message.charAt(counter));
+            }else{
+                int charPosition=ALPHABETS.indexOf(message.charAt(counter));
+                int keyValue = (charPosition+shiftKey)%25;
+
+                char cypherValue = ALPHABETS.charAt(keyValue);
+
+                cypherText.append(cypherValue);
+            }
+        }
+        String s = cypherText.toString();
+        return s;
+    }
 }
