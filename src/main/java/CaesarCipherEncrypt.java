f35099e510289abd24e80d4a126c819afdfc2098
Alex Karumbi <alex.muhia@student.moringaschool.com>
2021-09-05 20:17:03 +0300

    Add getters and setters

---
+++ src/main/java/CaesarCipherEncrypt.java
@@ -0,0 +1,9 @@
+public class CaesarCipherEncrypt {
+    private String message;
+    private int shiftKey;
+
+    public String getMessage(){return message;}
+    public int getShiftKey(){return shiftKey;}
+    public void setMessage(String message) { this.message = message; }
+    public void setShiftKey(int shiftKey) { this.shiftKey = shiftKey; }
+}
