0ab6f2d3070c165eafd37914e8aff4555ecfdd27
Alex Karumbi <alex.muhia@student.moringaschool.com>
2021-09-05 20:52:32 +0300

    Add variables for decryption

--- src/main/java/CaesarCipherDecrypt.java
+++ src/main/java/CaesarCipherDecrypt.java
@@ -1,5 +1,11 @@
 public class CaesarCipherDecrypt {
     public static String CaesarCipherDecryption(String s,int shiftKey){
+        final String ALPHABETS ="abcdefghijklmnopqrstuvwxyz";
+        s =s.toLowerCase();
+
+        StringBuilder cypherText =new StringBuilder();
+
+
 
     }
 }
